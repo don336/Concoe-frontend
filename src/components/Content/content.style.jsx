5d5631226330ef42bfe78af91e72d400b75c6bc9
don336 <smgmartinez92@gmail.com>
2023-04-13 18:57:00 +0300

    Added a content Component

--- src/components/Content/content.style.jsx
+++ src/components/Content/content.style.jsx
@@ -3,11 +3,37 @@ import {
   Container as muiContainer,
   Box,
   Button as muiButton,
+  Stack as muiStack,
+  Typography,
+  Stack,
 } from "@mui/material";
+import { COLORS } from "../../Styles/theme";
 
 export const StyledContainer = styled(muiContainer)`
   padding: 30px 15px 20px 20px;
   margin: auto;
+`;
+export const StyledTypography = styled(Typography)`
+  padding: 0.5rem;
+  color: ${COLORS.WHITE_SMOKE};
+  border-left: 3px solid ${COLORS.YELLOW_GREEN};
+`;
+export const MissionTypography = styled(Typography)`
+  padding: 0.5rem;
+  color: ${COLORS.WHITE_SMOKE};
+  margin-left: 1rem;
+`;
+export const TeamTypography = styled(Typography)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  text-align: center;
+  margin: auto;
+  margin-left: 1rem;
+  border-left: 3px solid ${COLORS.YELLOW_GREEN};
+
+  color: #333;
 `;
 export const StyledBox = styled(Box)`
   padding: 30px 0;
@@ -30,4 +56,22 @@ export const SecondButton = styled(muiButton)`
   border: 0.3px solid #333;
   font-size: 10px;
   width: 30%;
+`;
+
+export const MissionBox = styled(Stack)`
+  background: #3a3a3a;
+  border-radius: 7px;
+  width: 100%;
+  padding: 10px;
+  margin: auto;
+  margin-top: 5rem;
+`;
+export const TeamBox = styled(Box)`
+  background: ${COLORS.WHITE_SMOKE};
+  border-radius: 7px;
+  width: 100%;
+  color: ${COLORS.ERIE_BLACK};
+  padding: 10px;
+  margin: auto;
+  margin-top: 5rem;
 `;
--- src/features/Home/Home.styles.js
+++ src/features/Home/Home.styles.js
@@ -2,6 +2,5 @@ import { Container as muiContainer } from "@mui/material";
 import styled from "@emotion/styled";
 import { COLORS } from "../../Styles/theme";
 export const StyledContainer = styled(muiContainer)`
-  background: ${COLORS.ERIE_BLACK};
-  height: 200vh;
+  height: 300vh;
 `;
