9f98800f77f858240e41fb23c57fe5cbc2d18659
don336 <smgmartinez92@gmail.com>
2023-12-10 18:05:45 +0300

    updated code

---
+++ src/features/FarmingMethods/Farming_Methods.jsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+import Navbar from "../../layouts/Navbar/Navbar";
+
+Container;
+const Farming_Methods = () => {
+  return (
+    <Container maxWidth="xl">
+      <Navbar />
+    </Container>
+  );
+};
+
+export default Farming_Methods;
