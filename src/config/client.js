b708d30548e1b1dce3750fe2562477ac59ebfab5
Ssemugabi Martin <76908155+don336@users.noreply.github.com>
2024-12-03 21:32:41 +0300

    Update client.js

--- src/config/client.js
+++ src/config/client.js
@@ -1,6 +1,6 @@
 // export const baseUrl = `${process.env.REACT_APP_BASE_URL}api/v1/`
 
 // export const baseUrl = "http://Localhost:8000/api/v1/";
-export const baseUrl = "https://concoe-coffee-and-foods-production.up.railway.app/api/v1/";
+export const baseUrl = "https://personal-server-3fvv.onrender.com/api/v1/";
 
 // export const baseUrl = "https://personal-server-3fvv.onrender.com/api/v1/";
